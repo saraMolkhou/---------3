eda3f6dd009c789e61564474814cbd82617e9e20
saraMolkhou <saramolkhou0@gmail.com>
2024-06-07 11:52:38 +0300

    delete node-modules

--- ToDoListProject/Todo_ServerSide/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ ToDoListProject/Todo_ServerSide/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d34aad385c55ba5f51295e2334219602d82f62d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
